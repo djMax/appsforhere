824d3316d898f21fd2127e52fbbbcff74296e0fa
Max Metral <ma@x.com>
2015-02-02 23:44:36 -0500

    Add order export

--- controllers/reports.js
+++ controllers/reports.js
@@ -35,4 +35,72 @@ module.exports = function (router) {
                 res.json(rz);
             });
     });
+
+    router.get('/export/:start/:end', appUtils.auth, hasViewReportRole, function (req, res) {
+        var start = apiFormat(req.params.start), end = apiFormat(req.params.end);
+        var hasMore = true, csvRows = [[
+            'Invoice Id',
+            'Order Number',
+            'Invoice Date',
+            'Currency',
+            'Total',
+            'Status',
+
+        ]], raw = [], seen = {};
+
+        var doPage = function () {
+            req.user.hereApi().get({
+                tokens: req.user.tokens(),
+                url: req.user.hereApiUrl('invoices?maxResults=3000&endDate=' +
+                encodeURIComponent(end) + '&startDate=' + encodeURIComponent(start)),
+                json: true,
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            }, req.$eat(function (json) {
+                if (req.query.format === 'csv') {
+                    toArray(json, csvRows, seen);
+                } else {
+                    raw = raw.concat(json.invoices);
+                }
+                if (json.hasMoreResults) {
+                    console.log('HAS MORE');
+                    end = json.invoices[json.invoices.length-1].invoiceDate;
+                    doPage();
+                } else {
+                    if (req.query.format === 'csv') {
+                        for (var i = 0; i < csvRows.length; i++) {
+                            // This isn't the best way to generate this.
+                            csvRows[i] = csvRows[i].join(',');
+                        }
+                        res.type('text/csv').send(csvRows.join('\n'));
+                    } else {
+                        res.send('<html><body><pre>' + JSON.stringify(raw, null, '\t') + '</pre></body></html>');
+                    }
+                }
+            }));
+        };
+
+        doPage();
+    });
+
+    function toArray(json, rz, seen) {
+        json.invoices.forEach(function (inv) {
+            if (!seen[inv.invoiceID]) {
+                rz.push([
+                    inv.invoiceID,
+                    inv.number,
+                    inv.invoiceDate,
+                    inv.currencyCode,
+                    inv.total,
+                    inv.status
+                ]);
+                seen[inv.invoiceID] = true;
+            }
+        });
+    }
+
+    function apiFormat(str) {
+        return str + 'T00:00:00.000+0000';
+    }
 };
